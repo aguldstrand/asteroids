c2abbbf0745252bbd63ec823e5ae0aeb3947f218
Anders Guldstrand <anders.guldstrand@lbi.com>
2014-06-23 18:04:46 +0200

    more basic server stuffs

--- asteroids/GameLoop.js
+++ asteroids/GameLoop.js
@@ -1,9 +1,25 @@
 function GameLoop(options) {
 	this.sendGameState = options.sendGameState;
+	this.running = false;
 }
 
-GameLoop.prototype.start = function() {};
+GameLoop.prototype.start = function() {
+	this.running = true;
+	setTimeout(this.step.bind(this));
+};
+
+GameLoop.prototype.step = function() {
+	if (this.running) {
+		// setTimeout should have good enough resolution for a game loop
+		// and wont hammer the cpu the same way as setImmediate
+		setTimeout(this.step.bind(this));
+	}
+};
 
 GameLoop.prototype.addPlayer = function(id, options) {};
+
+GameLoop.prototype.removePlayer = function(id) {};
+
+GameLoop.prototype.userInput = function(id, buttonArray) {};
 
 module.exports = GameLoop;
